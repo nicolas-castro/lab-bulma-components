e041a02de253cd7a75927ddbbb8a4780382cbd21
Nicolas Castro <nicolascastro@me.com>
2019-03-21 21:13:53 -0400

    Added Form

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
-
+import FormField from "./FormField"
 
 class App extends Component {
 
-  render () {
-
-    return <NavBar />
-    
+  render () { 
+    return (
+      <div className="App">
+      <NavBar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+      </div>
+    )
   }
 }
 
