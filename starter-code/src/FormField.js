980a00ba088d98aa4fc9cbce6c7447e6b31a626c
Nicolas Castro <nicolascastro@me.com>
2019-03-23 16:27:02 -0400

    Form

--- starter-code/src/FormField.js
+++ starter-code/src/FormField.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FormField = (props) => {
   return (
-  <div className="field">
+  <div className="field column is-4 is-offset-one-quarter">
     <label className="label">{props.name}</label>
     <div className="control">
       <input className="input" type="text" placeholder={props.placeholder} />
